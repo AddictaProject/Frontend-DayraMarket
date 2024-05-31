4694779a51778685026d9804dd46388d0e72e8da
Mohamed said <kankgood@gmail.com>
2024-05-31 22:14:16 +0300

    interceptor and Products responsive fixed;

--- src/app/Components/layout/home/product-slider/product-slider.component.ts
+++ src/app/Components/layout/home/product-slider/product-slider.component.ts
@@ -2,14 +2,22 @@ import { Component } from '@angular/core';
 import { DragScrollComponent, DragScrollItemDirective } from 'ngx-drag-scroll';
 import { CardComponent } from "../../../Shared/card/card.component";
 
+import { HttpClient } from '@angular/common/http';
+import { ProductService } from '../../../../Services/ProductService/product.service';
+
 @Component({
     selector: 'app-product-slider',
     standalone: true,
     templateUrl: './product-slider.component.html',
     styleUrl: './product-slider.component.css',
-    imports: [DragScrollComponent,
-        DragScrollItemDirective, CardComponent]
+    imports: [DragScrollComponent,DragScrollItemDirective,CardComponent]
 })
 export class ProductSliderComponent {
+  constructor(private productService: ProductService,private httpClient: HttpClient) {
+    productService.getProducts().subscribe(products=>{
+      console.log(products);
+    });
+
+  }
 
 }
---
+++ src/app/Models/IProductPagination.ts
@@ -0,0 +1,6 @@
+import { IProduct } from "./IProduct";
+
+export interface IProductPagination {
+  result:IProduct[];
+  totalcount:number;
+}
