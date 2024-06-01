42ae66de3a5bf6b43bc8c3ee5cb3af1da26391e8
nada2277 <nraslan101@gmail.com>
2024-06-02 00:13:11 +0200

    Updates

--- src/app/Models/IProductDetails.ts
+++ src/app/Models/IProductDetails.ts
@@ -1,13 +1,46 @@
+
+
 export interface IProductDetails {
-    uuid:string;
-    displayName:string;
-    categoryDisplayName: string;
-    lowestPrice :number;
-    description:string;
-    averageRate:number;
-    photos:string[];
-    groupedVariants:any[];
-    selectedStock:any[];
+
+    photoPaths:string[];
+    selectedStock:IselectedStock;
     availableAttributes:string[];
+    product:IProductInDetails;
+}
+
+
+export interface IselectedStock {
+    // stockid
+    uuid:string;
+    price :number;
+    attributes:any[];
+
+}
+
+export interface IProductInDetails {
+
+    brandDisplayName:string;
+    categoryDisplayName: string;
+    description:string;
+    displayName:string;
+    groupedVariants:IgroupedVariants[];
+    lowestPrice :number;
+    // averageRate:number;
+
+}
+
+export interface IgroupedVariants {
+
+    productUuid:string;
+    attributeUuid: string;
+    values:Ivalues[];
+    attributeDisplayName:string;
+
+}
+
+export interface Ivalues {
+
+    uuid:string;
+    value:string;
 
 }
