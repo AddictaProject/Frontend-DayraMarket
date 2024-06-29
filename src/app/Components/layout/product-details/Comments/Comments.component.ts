d384f05e84b81a94cec7fd6ea07d719bc5a27a26
nada2277 <nraslan101@gmail.com>
2024-06-29 13:13:37 +0200

    Bar Of filtration in comments

--- src/app/Components/layout/product-details/Comments/Comments.component.ts
+++ src/app/Components/layout/product-details/Comments/Comments.component.ts
@@ -29,6 +29,16 @@ export class CommentsComponent implements OnInit {
     description: "sdksdjsk",
 
   }
+  vReview2 :IVendorReview ={
+    dateCreated :"",
+    rate: 2,
+    comment: "fkdjfkdjk",
+    userName: "nada",
+    description: "sdksdjsk",
+
+  }
+  ratingCount: { [key: number]: number } = {};
+  rating : number[]=[5,4,3,2,1]
 
   constructor(
     public _ProductDetailsService: ProductDetailsService,
@@ -40,17 +50,35 @@ export class CommentsComponent implements OnInit {
       filter(reviews => reviews.length > 0)
     ).subscribe(reviews => {
       this.vendorReview = reviews;
-      this.vendorReview.push(this.vReview ,this.vReview ,this.vReview,this.vReview,this.vReview);
+      this.vendorReview.push(this.vReview ,this.vReview ,this.vReview,this.vReview,this.vReview,
+        this.vReview2,this.vReview2
+      );
       this.remainNumOfComments =this.vendorReview.length-4;
-
+      
+      this.calculateRatings();
     }); 
     
-    
-
+  
   }
 
   showMore() {
-    this.showRemainCategories = !this.showRemainCategories;
+    this.showRemainCategories = !this.showRemainCategories; 
+  }
+
+  calculateRatings() {
+    this.ratingCount = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
+
+    this.vendorReview.forEach(review => {
+      if (this.ratingCount.hasOwnProperty(review.rate)) {
+        this.ratingCount[review.rate]++;
+      }
+    });
+
+    console.log(this.ratingCount);
+  }
+
+  getWidth(rating: number): string {
+    return this.vendorReview.length > 0 ? (this.ratingCount[rating] / this.vendorReview.length  * 100) + '%' : '0%';
   }
 
 }
