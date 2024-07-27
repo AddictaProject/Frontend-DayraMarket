13776489755479b3597394e9021e1c6bec0a6b98
Mohamed said <kankgood@gmail.com>
2024-07-27 23:46:57 +0300

    fix scrollPosition to top

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
@@ -7,7 +7,12 @@ import { HeadersInterceptor } from './headers.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: "top",
+      })
+    ),
     provideHttpClient(withInterceptorsFromDi()),{
       provide: HTTP_INTERCEPTORS,
       useClass: HeadersInterceptor,
